a7eaf2dffed53be9a70312dec2039eafe56d9aea
Alexander Much <alexandermuch@MacBook-Pro-von-Alexander-14.local>
2022-09-25 17:20:29 +0200

    contacts in progress

--- js/render.js
+++ js/render.js
@@ -255,6 +255,125 @@ function renderBoard() {
 }
 
 function renderAddTasks() {
+    console.log("test")
+    
+    document.querySelector('body').innerHTML = `
+
+    <nav class="kanban-navbar">
+    <div class="inner-container">
+        <img class="logo" src="img/form_logo/logo_white.png">
+
+        <div class="info">
+            <p class="name">Kanban Project Management Tool</p>
+            <img class="help" src="img/kanban_logo/help.png">
+            <img class="user-img" src="img/kanban_logo/user_example.png">
+        </div>
+    </div>
+</nav>
+
+<nav class="mobile-create-task-navbar">
+    <img class="mobile-logo-blue" src="img/form_logo/logo_blue.png">
+    <button class="create-task-mobile-btn">Create <img class="white-clear" src="img/kanban_logo/white_clear_btn.png"></button>
+</nav>
+
+
+<main class="kanban-main">
+    <div class="sidebar">
+        <div class="inner-sidebar">    
+            <div class="kanban-btns">
+                <div class="kanban-link" onclick="renderSummary()"><img class="kanban-img" src="img/kanban_logo/summary.png"><span>Summary</span></div>
+                <div class="kanban-link" onclick="renderBoard()"><img class="kanban-img" src="img/kanban_logo/board.png"><span>Board</span></div>
+                <div class="kanban-link" onclick="renderAddTasks()"><img  class="kanban-img" src="img/kanban_logo/addTask.png"><span>Add Task</span></div>
+                <div class="kanban-link"><img class="kanban-img" src="img/kanban_logo/contacts.png"><span>Contacts</span></div>
+            </div>
+
+            <div class="legal-notice">
+                <div class="kanban-link"><img class="kanban-img" src="img/kanban_logo/legal_notice.png"><span>Legal Notice</span></div>
+            </div>
+        </div>
+    </div>
+
+   
+
+    <div class="main-field">
+
+        <div class="add-task-container">
+            <div class="task-form-left">
+                <form class="task-form">
+                    <input class="input-title" type="text" placeholder="Enter a title">
+
+                    <div class="contacts-container">
+                        <select name="contacts" id="contacts">
+                            <option value="">select contacts to assign</option>
+                            <option value="">contact1</option>
+                            <option value="">contac2</option>
+                            <option value="">contact2</option>
+                        </select>
+                        <img class="arrow-select" src="img/kanban_logo/arrow_select.png" />
+                    </div>
+
+                    <div class="due-date-container">
+                        <label class="label-flex" for="due-date">Due date<input placeholder="dd/mm/yyyy" id="due-date"type="text"></label>
+                        <img class="date-img" src="img/kanban_logo/date_img.png" />
+                    </div>
+
+                    <div class="task-category-container">
+                        <select name="task-category" id="task-category">
+                            <option value="">assign task category</option>
+                            <option value="">New Category</option>
+                            <option value="">Sales</option>
+                            <option value="">Backoffice</option>
+                        </select>
+                        <img class="arrow-select" src="img/kanban_logo/arrow_select.png" />
+                    </div>
+
+                    <div class="priority-btn-container">
+                        <button class="urgent-btn">Urgent <img class="priority-urgent-img" src="img/kanban_logo/urgent-red.png"></button>
+                        <button class="medium-btn">Medium <img  class="priority-urgent-img" src="img/kanban_logo/middle-priority-orange.png"></button>
+                        <button class="non-urgent-btn">Low <img class="priority-urgent-img" src="img/kanban_logo/non-urgent-green.png"></button>
+                    </div>
+                    <div class="description-container">
+                        <span>Description</span>
+                        <textarea name="" id="textarea" placeholder="Enter a description"></textarea>
+                    </div>
+
+                    <div class="category-container">
+                        <label class="category-label" for="category">Subtask<input id="category" placeholder="Add new subtask" type="text"></label>
+                        <label for="subtask"><input type="checkbox" id="subtask"><span class="category-span">Subtask</span></label>
+                    </div>
+                </form>
+            </div>
+
+            <div class="task-form-right">
+                <div class="create-task-container">
+                    <button class="clear-btn">Clear <span class="close-x">X</span></button>
+                    <button class="create-task-btn">Create Task <img class="white-clear" src="img/kanban_logo/white_clear_btn.png"></button>
+                </div>
+
+            </div>
+        </div>
+
+
+
+
+
+
+        <div class="sidebar-mobile">
+            <div class="kanban-btns">
+                <div class="kanban-link"><img class="kanban-img" src="img/kanban_logo/summary.png"><span>Summary</span></div>
+                <div class="kanban-link"><img class="kanban-img" src="img/kanban_logo/board.png"><span>Board</span></div>
+                <div class="kanban-link"><img class="kanban-img" src="img/kanban_logo/addTask.png"><span>Add Task</span></div>
+                <div class="kanban-link"><img class="kanban-img" src="img/kanban_logo/contacts.png"><span>Contacts</span></div>
+            </div>
+        </div>
+
+    </div>
+</main>
+
+    `
+
+    
+
     // document.querySelector(".main-field").innerHTML =
 }
 
