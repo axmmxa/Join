271e50e73511dc8245b84a17aac14c3ed10d24ed
Emir Salihovic <e.salihovic@outlook.de>
2022-10-25 22:18:06 +0200

    add js files

---
+++ js/load.js
@@ -0,0 +1,12 @@
+setURL('https://gruppe-319.developerakademie.net/smallest_backend_ever')
+
+let users = [];
+
+async function init() {
+    await downloadFromServer();
+    users = JSON.parse(backend.getItem('users')) || [];
+}
+ 
+async function deleteUser(name) {
+  await backend.deleteItem('users');
+ }
---
+++ js/sign_up.js
@@ -0,0 +1,20 @@
+function signUp()  {
+    let email = document.getElementById('e-mail').value
+    let name = document.getElementById('name').value
+    let password = document.getElementById('password').value
+  
+    let user = {
+        'name': name,
+        'email': email,
+        'password': password
+    }
+  
+    addUser(user)
+    // window.location.href = '../login.html?msg=Du hast dich erfolgreich angemeldet'   
+  }
+  
+  function addUser(user) {
+    users.push(user)
+    backend.setItem('users', JSON.stringify(users));
+  }
+  
