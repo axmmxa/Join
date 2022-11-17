886b2bbbd575d1dcf3b2991a6bd77ce68ed13a93
Emir Salihovic <e.salihovic@outlook.de>
2022-11-17 13:35:32 +0100

    login function completed

--- js/login.js
+++ js/login.js
@@ -12,42 +12,36 @@ if (msg) {
 }
 
  async function login() {
-    let email = document.getElementById("e-mail").value
-    let password = document.getElementById("password").value
-    
-    
     await downloadFromServer();
     users = JSON.parse(backend.getItem('users')) || [];
-    
 
-    document.querySelector('.login-btn').addEventListener('click', ()=> {
-        console.log(users)
+    let email = document.getElementById("e-mail").value
+    let password = document.getElementById("password").value
+
+    console.log(users)
     loggedInUser = users.find(u => u.email == email && u.password == password)
-
-    console.log(loggedInUser)
-        if (loggedInUser) {
-            console.log("User gefunden") 
+       
+    if (loggedInUser) {
+        console.log("User gefunden") 
         saveLoggedInUser()
         window.location.href = './summary.html'
-        } 
-        
-    })
-
-    document.querySelector('.guest-btn').addEventListener('click', ()=> {
-        console.log("Guest login")
-        loggedInUser = {
-            "contacts": [],
-            "tasks": [],
-            "name": "Guest",
-            "email": "",
-            "password": "",
-            "user-background-color": "gray"
-        }
-        saveLoggedInUser()
-        localStorage.setItem("task_id", JSON.stringify(0))
-        window.location.href = './summary.html'
-    })
+    }   
 }
+
+document.querySelector('.guest-btn').addEventListener('click', () => {
+    console.log("Guest login")
+    loggedInUser = {
+        "contacts": [],
+        "tasks": [],
+        "name": "Guest",
+        "email": "",
+        "password": "",
+        "user-background-color": "gray"
+    }
+    saveLoggedInUser()
+    localStorage.setItem("task_id", JSON.stringify(0))
+    window.location.href = './summary.html'
+})
  
     
         
