742dc1fad3a2b74729b958edc29aff36d3086a08
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2022-11-20 12:08:22 -0300

    check user for sign up

--- js/sign_up.js
+++ js/sign_up.js
@@ -1,22 +1,46 @@
-function signUp()  {
-    let email = document.getElementById('e-mail').value
-    let name = document.getElementById('name').value
-    let password = document.getElementById('password').value
-  
-    let user = {
-        'name': name,
-        'email': email,
-        'password': password,
-        'tasks' : [],
-        'contacts' : [],
-        'user-background-color': ""
-    }
-    addUser(user)  
-  }
-  
+let email;
+let name;
+let password;
+
+function signUp() {
+  email = document.getElementById("e-mail").value;
+  name = document.getElementById("name").value;
+  password = document.getElementById("password").value;
+
+  let user = {
+    name: name,
+    email: email,
+    password: password,
+    tasks: [],
+    contacts: [],
+    "user-background-color": "",
+  };
+  addUser(user);
+}
+
 async function addUser(user) {
-    users.push(user)
-    await backend.setItem('users', JSON.stringify(users));
-    window.location.href = '../login.html?msg=Du hast dich erfolgreich angemeldet!' 
+  if (!checkUserExist) {
+    users.push(user);
+    await backend.setItem("users", JSON.stringify(users));
+    window.location.href =
+      "../login.html?msg=Du hast dich erfolgreich angemeldet!";
+  } else {
+    document.querySelector(".email-exist").classList.remove("d-none");
+    setTimeout(() => {
+      document.querySelector(".email-exist").classList.add("d-none");
+    }, 5000);
   }
-  
+}
+
+async function checkUserExist() {
+  await getUsersFromBackend();
+
+  for (let i = 0; i < users.length; i++) {
+    const element = users[i].email;
+    if (element == email) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
